a3f524cf80ff4b9803e95efddf86d97be3b9c916
zombiejuice97 <azan16shaikh@gmail.com>
2023-01-20 23:45:37 +0530

    create a task and save in the database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,39 @@
 //imports 
 
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const Task = require('../model/taskModel');
+
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('Home page!');
+})
+
+app.post('/api/tasks', async function (req, res) {
+    try {
+        const task = await Task.create(req.body);
+        res.json(task);
+    } catch (error) {
+        res.send(error);
+    }
+
+})
 
 
 
-app.listen(3000,function(){
-    console.log('Server running on port 3000!');
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(3000, function () {
+            console.log('Server running on port 3000!');
+        });
+
+    })
+    .catch((err) => console.log(err));
+
